88603abe8012cf70a2c183cdd3897c2280270686
Ian Darwin <ian@darwinsys.com>
2024-07-16 10:37:16 -0400

    Dummy export page

---
+++ lib/ui/export_track.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/// Activity for Export
+///
+class ExportPage extends StatefulWidget {
+
+  @override
+  ExportState createState() => Exporttate();
+
+}
+
+class ExportState extends State<ExportPage> {
+
+  @override
+  Widget build(var context) {
+    return Scaffold(
+        body:Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                const Text('Export not written yet'),
+                ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('OK'),
+                )
+              ]
+            )
+        )
+    );
+  }
+}
+
