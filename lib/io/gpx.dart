0cb0ed58ff6b9203df6f762b907d76f58e5d22b9
Ian Darwin <ian@darwinsys.com>
2024-08-31 14:59:50 -0400

    Print date+time w/ custom DateFormat

--- lib/io/gpx.dart
+++ lib/io/gpx.dart
@@ -1,4 +1,5 @@
 
+import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 import 'package:xml/xml.dart' as xml;
 
@@ -6,31 +7,38 @@ import 'package:jpstrack/model/track.dart';
 
 // GPX Writer, written by ChatGPT
 class Gpx {
+  // We stick the Z on arbitrarily because Dart's date format
+  // class apparently isn't capable of printing the actual tz
+  static DateFormat iso8166df = DateFormat("yyyy-MM-d'T'HH:mm:ss'Z'");
+
   static String buildGPXString(Track track) {
     xml.XmlBuilder builder = xml.XmlBuilder();
     builder.processing('xml', 'version="1.0" encoding="UTF-8"');
     builder.element('gpx',
         attributes: {
-        'xmlns':'http://www.garmin.com/xmlschemas/TrackPointExtension/v1',
-        'version': '1.1',
-        'creator': 'https://darwinsys.com/jpstrack',
-    }, nest: () {
-      builder.element('trk', nest: () {
-        builder.element('name', nest: 'Track ${track.id}');
-        builder.element('trkseg', nest: () {
-          for (var location in track.steps) {
-            builder.element('trkpt', attributes: {
-              'lat': location.latitude.toString(),
-              'lon': location.longitude.toString(),
-              'alt': location.altitude.toString(),
-            }, nest: () {
-              builder.element('time', nest: 
-                DateTime.fromMillisecondsSinceEpoch(location.time!.toInt()));
+          'xmlns':'http://www.garmin.com/xmlschemas/TrackPointExtension/v1',
+          'version': '1.1',
+          'creator': 'https://darwinsys.com/jpstrack',
+        }, nest: () {
+          builder.element('trk', nest: () {
+            builder.element('name', nest: 'Track ${track.id}');
+            builder.element('trkseg', nest: () {
+              for (var location in track.steps) {
+                builder.element('trkpt', attributes: {
+                  'lat': location.latitude.toString(),
+                  'lon': location.longitude.toString(),
+                  'alt': location.altitude.toString(),
+                }, nest: () {
+                  builder.element('time',
+                      nest: iso8166df.format(
+                        // location_platform_interface gives timestamp as a double
+                        // holding the # of mSec since the epoch.
+                        DateTime.fromMillisecondsSinceEpoch(location.time!.toInt())));
+                });
+              }
             });
-          }
+          });
         });
-      });
-    });
     return builder.buildDocument().toXmlString(pretty: true, indent: '  ');
   }
 }
