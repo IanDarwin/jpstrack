56ac7e2b17fc8667381f21879bc0f4c1927d9b72
Ian Darwin <ian@darwinsys.com>
2024-09-03 09:37:46 -0400

    Add Enum for upload destinations

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,22 @@
 class Constants {
 
-	// Keys for Settings
-	static const String KEY_AUTO_UPLOAD = "auto-upload";
-	static const String KEY_LOGIN_NAME = "loginName";
+  // Keys for Settings
+  static const String KEY_AUTO_UPLOAD = "auto-upload";
+  static const String KEY_LOGIN_NAME = "loginName";
 
   static const String URL_ABOUT = "https://darwinsys.com/jpstrack";
 
-	static const String URL_UPLOAD = "https://api.openstreetmap.org/";
+  static const String URL_UPLOAD = "https://api.openstreetmap.org/";
 
+}
+
+enum UploadDest {
+  OpenStreetMap('OpenStreetMap Main', 'api.openstreetmap.org'),
+  OpenStreetMapTest('OpenStreetMap Test Server',
+    'master.apis.dev.openstreetmap.org'),
+  CustomBasic('Custom URL, Basic Auth', '_'),
+  CustomOAuth2('Custom URL, OAuth2', '_');
+  final String name;
+  final String url;
+  const UploadDest(this.name, this.url);
 }
